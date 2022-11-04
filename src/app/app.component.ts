5218345224a5c0fe5154af900c8b0d6f824b74b6
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2022-11-04 11:10:40 +0100

    Create Simple App where we using directives, Event-binding, Interpolation, methods with parameters Array declaration with<string> type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tasks-list-example';
+  title = 'Tasks-list-example';
+  newTask: string = '';
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add() {
+    this.tasksList.push(this.newTask); //Two-Way-binded 'newTask' add to tasksList
+    this.newTask = ''; //remove added content of 'newTask'
+  }
+  remove(task: string) {
+    /* when we declare a method we need to remember to set apropriate type in parameters*/
+    this.tasksList = this.tasksList.filter((e) => e != task); //'filter' method return new array with elements other than 'task' parameter|we assign returned array to existing variable 'taskList in Component
+  }
+  done(task: string) {
+    /* when we declare a method we need to remember to set apropriate type in parameters*/
+    this.tasksDone.push(task); //add task to a new Array 'tasksDone'
+    this.remove(task); //remove this task from 'tasksList' with method above
+  }
 }
