5bce9f5cef2c8b7007b6939f336dbc08a37588ab
oleksy-piotr-angular <piotr.oleksy.angular@gmail.com>
2022-11-15 18:53:39 +0100

    add image as a dot of <li> changed somestyles to get my own apperance - implement directive code with manipulation of <li> element

--- src/app/shared/checked.directive.ts
+++ src/app/shared/checked.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[appChecked]'
+  selector: '[appChecked]',
 })
-export class CheckedDirective {
+export class CheckedDirective implements OnInit {
+  /* good habit to modify HTML elements in ngOnInit lifecycle hook method */
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    /*above was injected two properties from imported class */
+  }
 
+  ngOnInit(): void {
+    let li = this.el.nativeElement; //we assign this element <li> where this directive was implemented thanks 'ElementRef'
+    this.renderer.setStyle(
+      li,
+      'background',
+      'url(/assets/checked.png) 10px center no-repeat'
+    ); //above we use 'renderer' object to manipulate <li> element style but we used here this solution("background" not "listy-style-image") because we want to have checked.png aligned vertically center and 10px from left border|in this solution we had to add styles into DoneTask-Component|
+    this.renderer.setStyle(li, 'background-color', '#9ACD32');
+  }
 }
