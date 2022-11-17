b779cd7d19e063f8bf6419eb5b57f402d44c95a8
oleksy-piotr-angular <piotr.oleksy.angular@gmail.com>
2022-11-17 18:27:39 +0100

    Set Property-Binding in DateDirective to send {task.end} date to it. We needed to wrap additional <div> with *ngIf Directive to get id of errors because {end} is a optional property and it can be null/undefined but [date] do not recieve null/undefined values

--- src/app/shared/date.directive.ts
+++ src/app/shared/date.directive.ts
@@ -1,19 +1,22 @@
-import { Directive, HostListener } from '@angular/core';
-
+import { Directive, HostListener, Input } from '@angular/core';
+//Directives are simplified Components
 @Directive({
   selector: '[appDate]',
 })
 export class DateDirective {
+  @Input()
+  date: Date = new Date(); //must initiate all new properties...
+
   constructor() {}
 
   @HostListener('mouseenter') //we initiate @HostListener wich listen 'mouseenter' Event and Run method below
-  mouseE1nter(eventDate: Event): void {
-    //this method is running when '@HostListener' receive 'Event'-'mouseenter'
-    console.log('mouseenter');
+  mouseEnter(eventDate: Event): void {
+    //this method is running when '@HostListener' receive 'Event'-'mouseenter' and it can be named with another name 'mouseEnter' than Event 'mouseenter' as we See
+    console.log(this.date);
   }
   @HostListener('mouseleave') //we initiate @HostListener  which listen 'mouseleave' Event and Run method below
   mouseLeave(eventDate: Event): void {
-    //this method is running when '@HostListener' receive 'Event'- 'mouseleave'
-    console.log('mouseleave');
+    //this method is running when '@HostListener' receive 'Event'- 'mouseleave'  and it can be named with another name ' mouseLeave' than Event 'mouseleave' as we See
+    console.log(this.date);
   }
 }
--- src/app/todo-task/todo-task.component.ts
+++ src/app/todo-task/todo-task.component.ts
@@ -28,6 +28,7 @@ export class TodoTaskComponent implements OnInit {
     this.emitRemove.emit(task); //emit up to Parent AppComponent */
   }
   done(task: Task) {
+    task.end = new Date(); //we have to set Date 'end' before we pass this 'task' variable to method below
     this.tasksService.done(task); //we used Service method instead EventEmiter-emitDone(event-binding) below:
     /* //we 'emit/send' the data to be done from the todo-task.component to the method from Parent by executing this method(look at todo-task.component.html)
     this.emitDone.emit(task); //emit up to Parent AppComponent */
