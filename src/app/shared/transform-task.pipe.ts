8626920434fc391ca90cefee46e6508db772be21
oleksy-piotr-angular <oleksy.piotr.angular@gmail.com>
2022-11-17 21:07:13 +0100

    Implement String Transformation into Pipe trasformTask

--- src/app/shared/transform-task.pipe.ts
+++ src/app/shared/transform-task.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'transformTask',
 })
 export class TransformTaskPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return value;
+  transform(value: string, ...args: unknown[]): unknown {
+    return value.charAt(0).toUpperCase() + value.slice(1); //here we modify string | take first letter with charAt(0) and change it to upperCase and paste rest string from second letter with slice(1)
   }
 }
