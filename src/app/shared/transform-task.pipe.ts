ef7f355d6a6e3a64dac122bd7064aae02d0fec42
oleksy-piotr-angular <oleksy.piotr.angular@gmail.com>
2022-11-18 09:45:21 +0100

    Implements additional 'Argument' to display in transformPipe and put additional Notes about Pipe

--- src/app/shared/transform-task.pipe.ts
+++ src/app/shared/transform-task.pipe.ts
@@ -1,10 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+//Pipe change only Data displaying Data in FrontEnd - (Not modify data) Data are same as we Receive from 'Inputs'
 @Pipe({
   name: 'transformTask',
 })
 export class TransformTaskPipe implements PipeTransform {
-  transform(value: string, ...args: unknown[]): unknown {
-    return value.charAt(0).toUpperCase() + value.slice(1); //here we modify string | take first letter with charAt(0) and change it to upperCase and paste rest string from second letter with slice(1)
+  /* below we change value type on <string> and argument(args) sended after colon ':' where Pipe is assigned|'args' changed to optional(but we changed to empty string on default content) and switch type into <string> - our whole Pipe transform <string> */
+  transform(value: string, args: string = ''): unknown {
+    return value.charAt(0).toUpperCase() + value.slice(1) + args; //here we modify string | take first letter with charAt(0) and change it to upperCase and paste rest string from second letter with slice(1) and paste 'args' with additional string after colon in transformTask assignment {todo-task.component.html;done-task.component.html}
   }
 }
