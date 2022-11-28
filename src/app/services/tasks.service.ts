d4c85d90c33a4186af2e1f4ecf9747476291a43f
oleksy-piotr-angular <oleksy.piotr.angular@gmail.com>
2022-11-28 23:47:59 +0100

    remove unnecessary Array from TasksService and replace some [this.variables] into local variables

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -6,9 +6,9 @@ import { Task } from '../models/task'; // it must be if we use Interface <Task>
 export class TasksService {
   //everything was moved from 'app.component.ts' component to this 'Service'. TasksService has variables/fields/Arrays below and execute all operations on them but all events receives from child-components|child component deal with the display of data and receiveing tasks but not execute any operations
 
-  private tasksList: Array<Task> = []; // access to this Component only|replacing <string> with <Task> interface|
+  /** we can reduce code and do not initiate "taskList: Array<task> = []" */
   private tasksListObs = new BehaviorSubject<Array<Task>>( //|replacing <string> with <Task> interface|
-    this.tasksList
+    [] /**<- BehaviorSubject need to have some Array initialization but not need 'this.taskList' as was done before*/
   ); /*  Here BehaviorSubject was created to prepare to observe changes in TasksList[] */
 
   /* The BehaviorSubject has the characteristic that it stores the “current” value. This means that you can always directly get the last emitted value from the BehaviorSubject. */
@@ -16,7 +16,8 @@ export class TasksService {
 
   constructor() {
     //above <= changed from "ngOnInit(): void" after move from "app.component.ts"
-    this.tasksList = [
+    const tasksList = [
+      /** above we change 'this' variable to local 'taskList' variable*/
       //below replace <string> data with <Task> data(object JS)
       {
         name: 'wash the dishes',
@@ -46,19 +47,24 @@ export class TasksService {
       },
       /** above we change Date() into string and add property "isDOne" to send data to MongoDB Data Storage */
     ];
-    this.tasksListObs.next(this.tasksList); // 'this.tasksList' will be emited to subsribers
+    this.tasksListObs.next(tasksList); // 'tasksList' will be emited to subsribers/we propagate through 'BehaviourSubject'
   }
 
   add(task: Task): void {
-    //above replace <string> data with <Task> data(object JS)
+    /* //above replace <string> data with <Task> data(object JS)
     this.tasksList.push(task); //This method add task which received from 'task' parameter
-    this.tasksListObs.next(this.tasksList); //tasksList was updated above so we emit here its actual content to subsribers
+    this.tasksListObs.next(this.tasksList); //tasksList was updated above so we emit here its actual content to subsribers */
+    /** below we change 'this' variable to local 'taskList' variable*/
+    const list = this.tasksListObs.getValue(); // we return all values from 'Observable' and put them into this const variable
+    list.push(task); // add new element from this methods parameter
+    this.tasksListObs.next(list); // finally propagate this list through 'Observable' through 'next()' method
   }
   remove(task: Task) {
     //above replace <string> data with <Task> data(object JS)
     /* when we declare a method we need to remember to set apropriate type in parameters*/
-    this.tasksList = this.tasksList.filter((e) => e != task); //'filter' method return new array with elements other than 'task' parameter|we assign returned array to existing variable 'taskList in Component
-    this.tasksListObs.next(this.tasksList); //tasksList was removed above so we emit here its actual content to subsribers
+    /** below we change 'this.taskList' into local variable 'taskList' and filter list from 'Observable' values returned with method 'getValue()'*/
+    const tasksList = this.tasksListObs.getValue().filter((e) => e != task); //'filter' method return new array with elements other than 'task' parameter|we assign returned array to existing variable 'taskList in Component
+    this.tasksListObs.next(tasksList); //tasksList was removed above so we emit here its actual content to subsribers
   }
   done(task: Task) {
     //above replace <string> data with <Task> data(object JS)
@@ -70,6 +76,7 @@ export class TasksService {
     task.end =
       new Date().toLocaleString() /** change 'Date' into String if we want to Send data appropriate with MongoDB settings */; //we have to set Date 'end' before we pass this 'task' variable to method below
     task.isDone = true;
+    /** below we change 'this' variable to local 'taskList' variable*/
     const list = this.tasksListObs.getValue(); // we return all values and put them into this const variable
     this.tasksListObs.next(list); // with method 'next()' we put again all Values from 'list' variable and send info to subscribers that we have new elements in Subject
     /** we do 'const list' because this we cannot inform that some property in Subject was changed we need to again assaign the same list */
--- src/app/todo-task/todo-task.component.ts
+++ src/app/todo-task/todo-task.component.ts
@@ -14,7 +14,7 @@ export class TodoTaskComponent implements OnInit {
       this.tasksList = tasks
         .filter(
           (t) => t.isDone === false
-        ) /**this filter method recieve Array where 'isDone' = false(Boolean_flag) */
+        ) /**this filter method recieve Array where 'isDone' = false(Boolean_flag) and return whole Array to local 'this.taskList'*/
         .slice(); //1. after subsribing actual state of 'tasksList' Array<string> from TasksService we assign it to internal variable 'taksList' instead Using @Input() (property-binding) as below(we don't neeed to pass data from Parent because Everything is in 'TasksService'):
       //2. 'tasks' we receive from Service but Angular cannot detect if something in 'taskList' appeared (new element) because new element is added only to Service 'tasks' - thats why sortName Pipe on tasksList not work properly|Angular checks if tasksList has new reference (new Array) but we have same reference 'tasks' all the time. Thats why we use slice() method to return a new Copy of Array reference (return new reference of 'tasks'<Array> from service) and Angular detect that new Array(new reference of Array) is here and sortName Pipe will be invoke properly after adding new element to list through TasksService
     });
