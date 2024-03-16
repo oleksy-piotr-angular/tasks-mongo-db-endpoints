11fe48f6ce298202516f4c2ac6f725851b76682f
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2024-03-16 17:30:43 +0100

    InitSpecs Files for Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { TodoTaskComponent } from './todo-task/todo-task.component';
 import { DoneTaskComponent } from './done-task/done-task.component';
 import { CheckedDirective } from './shared/checked.directive';
 import { DateDirective } from './shared/date.directive';
-import { TransformTaskPipe } from './shared/transform-task.pipe';
-import { SortNamePipe } from './shared/sort-name.pipe';
+import { TransformTaskPipe } from './shared/pipes/TransformTask/transform-task.pipe';
+import { SortNamePipe } from './shared/pipes/SortName/sort-name.pipe';
 import { HttpService } from './services/http.service';
 import { TasksService } from './services/tasks.service';
 
--- src/app/shared/pipes/SortName/sort-name.pipe.ts
+++ src/app/shared/pipes/SortName/sort-name.pipe.ts
@@ -1,11 +1,11 @@
-import { Task } from './../models/task';
+import { Task } from '../../../models/task';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'sortName',
 })
 export class SortNamePipe implements PipeTransform {
-  transform(value: Task[], args?: any): Task[] {
+  transform(value: Task[]): Task[] {
     return value.sort((a, b) => {
       return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
     });
